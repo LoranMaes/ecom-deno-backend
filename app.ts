e9547c47498b885d6955c8c39d2396e8f16ebaaf
Loran Maes <loran.maes@student.odisee.be>
2024-01-23 17:38:33 +0100

    Update PORT to 8000

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./controllers/routes/routes.ts";
 
 const app = new Application();
-const PORT = 8080;
+const PORT = 8000;
 
 app.use(router.routes());
 app.use(router.allowedMethods());
