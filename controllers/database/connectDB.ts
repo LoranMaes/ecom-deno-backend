cc14e696d0b065949b218357ba23b86f565cefd2
Loran Maes <loran.maes@student.odisee.be>
2024-01-24 16:54:31 +0100

    Refactor database connection code

--- controllers/database/connectDB.ts
+++ controllers/database/connectDB.ts
@@ -1,10 +1,9 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 import { load } from "https://deno.land/std@0.212.0/dotenv/mod.ts";
 
+const client = new MongoClient();
 const env = await load();
 const dbString = `mongodb+srv://loranmaes:${env["MONGO_DB_PASSWORD"]}@cluster0.jljswyp.mongodb.net/?authMechanism=SCRAM-SHA-1`;
-
-const client = new MongoClient();
 try {
   await client.connect(dbString);
   console.log("Database connected");
